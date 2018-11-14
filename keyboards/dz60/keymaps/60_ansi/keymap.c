6625e3e50dbe1b0ece8fe3edacc66edada4d54cd
Nikolas Davis <nikdavis@pdx.edu>
2018-11-14 02:00:37 -0800

    My keyboard layout

--- keyboards/dz60/keymaps/60_ansi/keymap.c
+++ keyboards/dz60/keymaps/60_ansi/keymap.c
@@ -1,5 +1,36 @@
 #include QMK_KEYBOARD_H
 
+#define ______   KC_TRNS
+
+enum custom_keycodes {
+    GITPL = SAFE_RANGE,
+    GITPS,
+	MAGIC
+};
+
+// Note: I use software keyboard transation, so these commands are
+// mapped as if I were typing on dvorak on qwerty
+// 1 -> git pull
+// 2 -> git push
+// 3 -> surprise command
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        switch(keycode) {
+            case GITPL:
+                SEND_STRING("ugk rfpp"SS_TAP(X_ENTER));
+                return false;
+            case GITPS:
+				SEND_STRING("ugk rf;j"SS_TAP(X_ENTER));
+                return false;
+			case MAGIC:
+				SEND_STRING("gkq; maugi!");
+				return false;
+        }
+    }
+    return true;
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	LAYOUT_60_ansi(
@@ -7,12 +38,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          MO(1),   KC_RCTL),
+		KC_LCTL, KC_LALT,          KC_LGUI,                  KC_SPC,                             MO(1),   MO(2),            KC_LEFT, KC_RIGHT),
 
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
-		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_HOME, KC_PGUP, KC_TRNS,
-		KC_TRNS,          KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_DEL,  KC_END,  KC_PGDN,          KC_TRNS,
-		KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS)
+		______,           RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ______,  KC_PSCR, KC_SLCK, KC_PAUS, RESET,
+		______,           ______,  KC_MRWD, KC_MFFD, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, ______,  KC_INS,  KC_HOME, KC_PGUP, ______,
+		______,           GITPL,   GITPS,   MAGIC,   ______,  ______,  ______,  ______,  KC_DEL,  KC_END,  KC_PGDN,          ______,
+		______, ______,             ______,                  ______,                              ______,  ______,           ______,  ______),
+	
+	LAYOUT_60_ansi(
+		______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,           KC_POWER,
+		______,           ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,
+		______,           ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,
+		______,           ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,           ______,
+		______,  ______,           KC_LGUI,                  ______,                              ______,  ______,           ______,  ______)
 };
