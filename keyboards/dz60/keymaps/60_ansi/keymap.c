d2ab10417196114d8a1ac47aa2b6486c5efe9307
Nikolas Davis <nikdavis@pdx.edu>
2020-02-19 23:07:19 -0800

    Finalize arrows, lock, misc

--- keyboards/dz60/keymaps/60_ansi/keymap.c
+++ keyboards/dz60/keymaps/60_ansi/keymap.c
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-		KC_LCTL, KC_LALT,          KC_LGUI,                  KC_SPC,                             MO(1),   MO(2),            KC_LEFT, KC_RIGHT),
+		KC_LCTL, KC_LALT,          KC_LGUI,                  KC_SPC,                              MO(1),   KC_UP,            KC_LEFT, KC_RIGHT),
 
 	LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		______,           RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, ______,  KC_PSCR, KC_SLCK, KC_PAUS, RESET,
 		______,           ______,  KC_MRWD, KC_MFFD, KC_MPLY, KC_VOLD, KC_VOLU, KC_MUTE, ______,  KC_INS,  KC_HOME, KC_PGUP, ______,
 		______,           GITPL,   GITPS,   MAGIC,   ______,  ______,  ______,  ______,  KC_DEL,  KC_END,  KC_PGDN,          ______,
-		______, ______,             ______,                  ______,                              ______,  ______,           ______,  ______),
+		LOCK,   ______,             ______,                  ______,                              ______,  KC_DOWN,          KC_HOME, KC_END),
 	
 	LAYOUT_60_ansi(
 		______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,           LOCK,
